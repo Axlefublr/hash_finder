51e155bad6fe705ac16062413f802054642dcd20
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-09-01 18:15:12 +0800

    impl functionality

--- src/args.rs
+++ src/args.rs
@@ -3,5 +3,10 @@ use clap::Parser;
 #[derive(Parser)]
 #[command(author, about, next_line_help = true)]
 pub struct Args {
-
+	/// The amount of consecutive zeros at the end of the hash
+	#[arg(short = 'N')]
+	pub zeros_count: usize,
+	/// The amount of results you want to print to stdout
+	#[arg(short = 'F')]
+	pub max_results_count: u32,
 }
