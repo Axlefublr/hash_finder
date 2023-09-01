2a5b3be25f94aca2e5df0ed0fa8db458895be453
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-09-01 22:21:46 +0800

    clean up cli usage

--- src/args.rs
+++ src/args.rs
@@ -1,12 +1,12 @@
 use clap::Parser;
 
 #[derive(Parser)]
-#[command(author, about, next_line_help = true)]
+#[command(about, version)]
 pub struct Args {
 	/// The amount of consecutive zeros at the end of the hash
-	#[arg(short = 'N')]
+	#[arg(short = 'N', value_name = "ZEROS")]
 	pub zeros_count: usize,
 	/// The amount of results you want to print to stdout
-	#[arg(short = 'F')]
+	#[arg(short = 'F', value_name = "RESULTS")]
 	pub max_results_count: u32,
 }
