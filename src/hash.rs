2aa08c8dae8e5a6a2f2fce0e0fb8895f1e7aef1e
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-09-02 00:35:37 +0800

    add public function documentation

--- src/hash.rs
+++ src/hash.rs
@@ -2,6 +2,8 @@ use ring::digest::{Context, SHA256};
 use std::sync::{Arc, Mutex};
 use std::thread::{self, JoinHandle};
 
+use crate::MAX_ZEROS;
+
 fn create_sha256(input: String) -> String {
 	let mut context = Context::new(&SHA256);
 	context.update(input.as_bytes());
@@ -10,7 +12,28 @@ fn create_sha256(input: String) -> String {
 	hex::encode(digest.as_ref())
 }
 
-pub fn multithread_hashing(zeros_count: usize, max_results_count: u32) {
+/// Uses multiple threads to find your amount of hashes that have your amount of trailing zeros. Prints them to stdout.
+///
+/// # Arguments
+/// * `zeros_count` - The amount of consecutive trailing zeros in the hashes
+/// * `max_results_count` - The amount of matching hashes to find and print
+///
+/// # Usage
+/// ```
+/// let result = multithread_hashing(args.zeros_count, args.max_results_count);
+/// match result {
+///     Ok(()) => ExitCode::SUCCESS,
+///     Err(message) => {
+///         eprintln!("{}", message);
+///         ExitCode::FAILURE
+///     }
+/// }
+/// ```
+pub fn multithread_hashing(zeros_count: usize, max_results_count: u32) -> Result<(), &'static str> {
+	if zeros_count > MAX_ZEROS {
+		return Err("the hash string is 64 symbols long. it will never contain 65 trailing zeros");
+	}
+
 	let counter = Arc::new(Mutex::new(1));
 	let found_hashes = Arc::new(Mutex::new(0));
 
@@ -25,6 +48,7 @@ pub fn multithread_hashing(zeros_count: usize, max_results_count: u32) {
 	for handle in handles {
 		handle.join().unwrap();
 	}
+	Ok(())
 }
 
 fn spawn_thread(counter: &Arc<Mutex<u32>>, found_hashes: &Arc<Mutex<u32>>, zeros_count: usize, max_results_count: u32) -> JoinHandle<()> {
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
+use std::process::ExitCode;
 use args::Args;
 use clap::Parser;
 
 mod args;
 mod hash;
 
-fn main() {
+const MAX_ZEROS: usize = 64;
+
+fn main() -> ExitCode {
 	let args = Args::parse();
-	hash::multithread_hashing(args.zeros_count, args.max_results_count);
+	let result = hash::multithread_hashing(args.zeros_count, args.max_results_count);
+	match result {
+		Ok(()) => ExitCode::SUCCESS,
+		Err(message) => {
+			eprintln!("{}", message);
+			ExitCode::FAILURE
+		}
+	}
 }
